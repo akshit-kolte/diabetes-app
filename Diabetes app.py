a11421b560497d440c931b0beb81780ed856c299
akshit-kolte <akshitkolte9026@gmail.com>
2025-11-29 23:51:25 +0530

    Refactor diabetes prediction app input and prediction logic

--- Diabetes app.py
+++ Diabetes app.py
@@ -6,19 +6,19 @@ import streamlit as st
 
 model=joblib.load(open("logistic_regression_model (1).joblib", 'rb'))
 
-st.title("Diabetes app")
+st.title("Diabetes Prediction app")
 #Input feature
-Pregnancies=st.number_input("Pregnancies",min_value=0.0)
-Glucose=st.number_input("Glucose",min_value=0.0)
-BloodPressure=st.number_input("BloodPressure",min_value=0.0)
-SkinThickness=st.number_input("SkinThickness",min_value=0.0)
-Insulin=st.number_input("Insulin",min_value=0.0)
+Age=st.number_input("Age of the person",min_value=0.0)
 BMI=st.number_input("BMI",min_value=0.0)
+BloodPressure=st.number_input("Newspaper Adv Budget",min_value=0.0)
+Insulin=st.number_input("Insulin",min_value=0.0)
+Glucose=st.number_input("Glucose",min_value=0.0)
+SkinThickness=st.number_input("SkinThickness",min_value=0.0)
 DiabetesPedigreeFunction=st.number_input("DiabetesPedigreeFunction",min_value=0.0)
-Age=st.number_input("Age",min_value=0.0)
+Pregnancies=st.number_input("Pregnancies",min_value=0.0)
 
 #Make Pred
-if st.button('Predict Diabetes'):
-	input_data=np.array([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
-	prediction=model.predict(input_data)[0]
-    st.success(f'Predict Diabetes:{prediction:.2f}')
+if st.button('Diabetes Prediction'):
+	input_data=np.array([[Age,BMI,BloodPressure,Insulin,Glucose,SkinThickness,DiabetesPedigreeFunction,Pregnancies]])
+	prediction_model=model.predict(input_data)[0]
+	st.success(f'Predict Diabetes:{prediction:.2f}')
