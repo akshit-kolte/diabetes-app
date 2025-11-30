471d0832b74c474b2f257b19f134a8561d312cb6
akshit-kolte <akshitkolte9026@gmail.com>
2025-11-30 10:26:49 +0530

    Update Diabetes app.py

--- Diabetes app.py
+++ Diabetes app.py
@@ -19,6 +19,6 @@ Pregnancies=st.number_input("Pregnancies",min_value=0.0)
 
 #Make Pred
 if st.button('Diabetes Prediction'):
-	input_data=np.array([[Age, BMI, BloodPressure, Insulin, Glucose, SkinThickness, DiabetesPedigreeFunction, Pregnancies]])
-	prediction=model.predict(input_data)[0]
-	st.success(f'Predict Diabetes:{prediction:.2f}')
+	 input_data=np.array([[Age, BMI, BloodPressure, Insulin, Glucose, SkinThickness, DiabetesPedigreeFunction, Pregnancies]])
+	 prediction=model.predict(input_data)[0]
+	 st.success(f'Predict Diabetes:{prediction:.2f}')
